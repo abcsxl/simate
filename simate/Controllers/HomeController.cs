8c77185817ffceecb6f5190f052623293103c83f
SIM <abcsxl@msn.com>
2018-07-15 23:30:39 +0800

    about

--- simate/Controllers/HomeController.cs
+++ simate/Controllers/HomeController.cs
@@ -37,7 +37,10 @@ namespace simate.Controllers
                     "<p>" +
                     "Wuhan SIMATE Bearing Co., Ltd. is a professional company that sells bearings and related products.<br />" +
                     "Founded in 2006, SIMATE is committed to providing a full range of bearing one-stop solutions. The company's own trademark \"SIMATE &reg;\" is a more reliable and high-quality bearing product for users, creating more value for customers. There are over 20,500 models of stock in stock in 20 major categories. With dedicated inventory for industry customer needs, we are always ready to meet the professional needs of our customers. " +
-                    "</p>" +
+                    "</p>"
+                    ;
+
+                ViewData["leading products"] =
                     "<p>" +
                     "	◎leading products" +
                     "   <ul>" +
@@ -46,7 +49,10 @@ namespace simate.Controllers
                     "		<li>Domestic precision bearing series: A reliable solution to replace imported bearings with high-quality domestic bearings, saving manufacturing costs and improving market competitiveness for customers.</li> " +
                     "		<li>Special bearing series: one-way bearing series, all kinds of self-lubricating bearing sleeves, linear optical axes, guide rails, lead screws, bearing housings, steel balls, needle rollers, rollers, lubricating grease and other bearing related products are available. Can undertake a variety of non-standard bearings and related parts.</li> " +
                     "	</ul> " +
-                    "</p> " +
+                    "</p> "
+                    ;
+
+                ViewData["business philosophy"] =
                     "<p> " +
                     "	◎business philosophy" +
                     "	<ol> " +
@@ -65,16 +71,22 @@ namespace simate.Controllers
                     "<p>" +
                     "武汉驷马特轴承有限公司是一家专业销售轴承及相关产品的公司企业。<br />" +
                     "驷马特成立于2006年，致力于提供全系列轴承一站式解决方案。公司自有商标“SIMATE &reg;”，为用户更可靠高品质轴承产品，为客户创造更多价值。现备有20大类系列的4500多种型号现货库存。针对行业客户需求配备专用库存，随时满足客户的专业使用需求。" +
-                    "</p>" +
+                    "</p>"
+                    ;
+
+                ViewData["leading products"] =
                     "<p>" +
                     "    ◎主导产品" +
                     "    <ul>" +
                     "        <li>精密微型球轴承：内径10mm以下的微型向心球轴承、推力球轴承、调心球轴承等型号规格齐全，备有大量现货。满足仪表、自动化、微型电机等客户的广泛需求。</li>" +
                     "        <li>高品质进口轴承品牌：瑞典SKF、德国FAG、INA、日本NSK、NTN、NACHI、IKO、NMB、ASAHI、美国McGILL等等精密高品质轴承。满足客户的高端严苛要求应用环境。</li>" +
                     "        <li>国产精密轴承系列：高品质国产轴承替代进口轴承的可靠解决方案，为客户节约制造成本，提升市场竞争力。</li>" +
-                    "        <li>特种轴承系列：单向轴承系列、各类自润滑轴承套、直线光轴、导轨、丝杠、轴承座、钢球、滚针、滚子、润滑油脂等轴承相关配套产品皆可提供。可承接定做各种非标轴承及相关零件。</li>" +
+                    "        <li>特种轴承系列：单向轴承系列、各类自润滑轴承套、直线光轴、导轨、丝杠、轴承座、钢球、滚针、滚子、润滑油脂等轴承相关配套产品皆可提供。承接定做各种非标轴承及相关零件。</li>" +
                     "    </ul>" +
-                    "</p>" +
+                    "</p>"
+                    ;
+
+                ViewData["business philosophy"] =
                     "<p>" +
                     "    ◎经营理念" +
                     "    <ol>" +
